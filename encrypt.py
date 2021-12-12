aa7369b7b8fcc24991933ca46251f9038de41475
AdvaithGS <77888014+AdvaithGS@users.noreply.github.com>
2021-12-12 22:50:45 +0530

    added encryption method

--- encrypt.py
+++ encrypt.py
@@ -7,3 +7,15 @@ for i in range(len(text)-1):
         text = text[:i+1] + 'x' + text[i+1:]
 if len(text)%2 != 0:
     text += 'x'
+lst = []
+for i in range(0,len(text),2):
+    lst.append(text[i]+text[i+1])
+encrypted = ''
+for i in lst:
+    if get_coords(i[0])[0] == get_coords(i[1])[0]:
+        encrypted += get_letter([(get_coords(i[0])[0]-1)%5,get_coords(i[0])[1]]) + get_letter([(get_coords(i[1])[0]-1)%5,get_coords(i[1])[1]])
+    elif get_coords(i[0])[1] == get_coords(i[1])[1]:
+        encrypted += get_letter([get_coords(i[0])[0],(get_coords(i[0])[1]+1)%5]) + get_letter([get_coords(i[1])[0] , (get_coords(i[1])[1]+1)%5])
+    else:
+        encrypted += get_letter([get_coords(i[1])[0],get_coords(i[0])[1]]) + get_letter([get_coords(i[0])[0],get_coords(i[1])[1]])
+print(encrypted)
